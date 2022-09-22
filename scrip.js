24bc717d78cc7ead5f917ab4b147e2e22a8cef61
viet <viet20000333@gmail.com>
2022-09-22 16:32:11 +0700

    first commit

---
+++ scrip.js
@@ -0,0 +1,16 @@
+
+var bar = document.getElementById('bar');
+ bar.onclick = function () {
+   document.getElementById('navbar').style.right = '0px'; 
+
+ }
+
+var close =  document.getElementById('close');
+
+close.onclick = function () {
+   document.getElementById('navbar').style.right = '-300px'; 
+
+ }
+
+
+
